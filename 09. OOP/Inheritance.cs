e174b6c3f3a29e9601d087fa4c4c4424a259b85d
lee hyoseok <lhs19960330@gmail.com>
2024-01-09 16:52:41 +0900

    docs : 객체지향프로그래밍 (1)

---
+++ 09. OOP/Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._OOP
+{
+    internal class Inheritance
+    {
+        /**********************************************************************************
+        * 상속 (Inheritance)
+        *
+        * 부모클래스의 모든 기능을 가지는 자식클래스를 설계하는 방법
+        * is-a 관계 : 부모클래스가 자식클래스를 포함하는 상위개념일 경우 상속관계가 적합함
+        **********************************************************************************/
+
+        // <상속>
+        // 부모클래스를 상속하는 자식클래스에게 부모클래스의 모든 기능을 부여
+        // class 자식클래스 : 부모클래스
+
+
+    }
+}
